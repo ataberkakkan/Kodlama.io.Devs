9bdd236a48bb4e6dda86796d737d267963314d16
ataberkakkan <akkanataberk@gmail.com>
2023-11-27 14:34:31 +0300

    Register

--- src/demoProjects/kodlama.io.Devs/Persistence/PersistenceServiceRegistration.cs
+++ src/demoProjects/kodlama.io.Devs/Persistence/PersistenceServiceRegistration.cs
@@ -20,8 +20,13 @@ namespace Persistence
             services.AddDbContext<BaseDbContext>(options =>
                                                      options.UseSqlServer(
                                                          configuration.GetConnectionString("KodlamaioDevsConnectionString")));
+
             services.AddScoped<ILanguageRepository, LanguageRepository>();
             services.AddScoped<ITechnologyRepository, TechnologyRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUserOperationClaimRepository, UserOperationClaimRepository>();
+            services.AddScoped<IOperationClaimRepository, OperationClaimRepository>();
+            services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
 
             return services;
         }
